b38b9c8f31dac0402101524ee07fdcebc8fdda0b
Alex Michailidis <alekos_l2@hotmail.com>
2020-06-27 19:56:54 +0300

    keep track of all the given usernames so we can prevent duplicates

--- server/index.js
+++ server/index.js
@@ -1,11 +1,34 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var fs = require('fs')
+
+let usernames;
+try {
+    usernames = JSON.parse(fs.readFileSync('usernames.json'))
+} catch (error) {
+    fs.writeFileSync('usernames.json', JSON.stringify([]))
+    usernames = []
+}
+usernames = new Set(usernames);
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/available', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    const { username } = req.query
+
+    if (username && !usernames.has(username)) {
+        usernames.add(username)
+        fs.writeFileSync('usernames.json', JSON.stringify(Array.from(usernames)))
+        res.json(true)
+    } else {
+        res.json(false)
+    }
+});
 const broadCastNumberOfClients = () => {
     const numberOfClients = Object.keys(io.sockets.connected).length;
     io.emit("ONLINE_COUNT", numberOfClients)
@@ -17,9 +40,10 @@ io.on('connection', (socket) => {
     broadCastNumberOfClients();
 
     socket.on('disconnect', () => broadCastNumberOfClients())
-    socket.on('SEND_MESSAGE', data => {
+    socket.on('SEND_MESSAGE', (data) => {
         console.log(data)
         io.emit("UPDATE_MESSAGE_LIST", data)
+
     })
 });
 
